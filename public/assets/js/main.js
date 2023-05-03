a1a1e92b4ac5f45c6eb8d5a48b39a672c63b73cf
Abdalrhman M. Alkady <alkady2019@gmail.com>
2023-05-03 08:44:57 +0300

    Edit JS Function that responsible for handling card click event to add red box shadow
    Solved:
    When click Edit button function was working with it but solved Now!

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -9,35 +9,38 @@ $(document).ready(function() {
         var checkbox = $(this).find('input[type="checkbox"]');
         
         // Handle card click event
-        $(this).click(function(event) {
-          // Toggle the checkbox state
-          checkbox.prop('checked', !checkbox.prop('checked'));
-          
-          if (!checkbox.prop('checked')) {
-            // Remove the shadow when the checkbox is unchecked
-            $(this).css("box-shadow", "");
-          } else {
-            // Toggle the card shadow
-            $(this).css("box-shadow", "rgb(234 13 13 / 70%) 0px 0px 0px 0.25rem");
-          }
+        $(this).on('click', function(event) {
+            if (!$(event.target).is('a')) { // exclude <a> elements
+                // Toggle the checkbox state
+                checkbox.prop('checked', !checkbox.prop('checked'));
+                
+                if (!checkbox.prop('checked')) {
+                    // Remove the shadow when the checkbox is unchecked
+                    $(this).css("box-shadow", "");
+                } else {
+                    // Toggle the card shadow
+                    $(this).css("box-shadow", "rgb(234 13 13 / 70%) 0px 0px 0px 0.25rem");
+                }
+            }
         });
         
         // Handle checkbox change event
         checkbox.change(function(event) {
-          if (!checkbox.prop('checked')) {
-            // Remove the shadow when the checkbox is unchecked
-            $(this).parent().parent().parent().css("box-shadow", "");
-          } else {
-            // Toggle the card shadow
-            $(this).parent().parent().parent().css("box-shadow", "rgb(234 13 13 / 70%) 0px 0px 0px 0.25rem");
-          }
+            if (!checkbox.prop('checked')) {
+                // Remove the shadow when the checkbox is unchecked
+                $(this).parent().parent().parent().css("box-shadow", "");
+            } else {
+                // Toggle the card shadow
+                $(this).parent().parent().parent().css("box-shadow", "rgb(234 13 13 / 70%) 0px 0px 0px 0.25rem");
+            }
         });
         
         // Stop checkbox click event from bubbling up to card element
         checkbox.click(function(event) {
-          event.stopPropagation();
+            event.stopPropagation();
         });
-      });
+    });
+    
       
     
 });
