816b4bffec86059a41fef4a8fda6c737f5af2760
Phoenix-H22 <alkady2019@gmail.com>
2025-01-03 18:00:16 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,8 +7,7 @@ require_once dirname(__DIR__) . '/config/app.php';
 define('ROOT', dirname(__DIR__));
 $scriptDir = dirname($_SERVER['SCRIPT_NAME']);
 $baseDir = ($scriptDir === '/' || $scriptDir === '\\') ? '' : $scriptDir;
-
-$scheme = $_SERVER['REQUEST_SCHEME'] ?? ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? 'http'); // Default to 'http' if neither exists
+$scheme = $_SERVER['REQUEST_SCHEME'] ?? ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? $_SERVER['HTTPS'] == "on" ? 'https' : "http"); // Default to 'http' if neither exists
 $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
 $scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
 $baseDir = ($scriptDir === '/' || $scriptDir === '\\') ? '' : $scriptDir;
