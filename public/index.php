7866a82033dd8fef191ee6cc0486773473be9376
Phoenix-H22 <alkady2019@gmail.com>
2025-01-03 17:43:52 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ define('ROOT', dirname(__DIR__));
 $scriptDir = dirname($_SERVER['SCRIPT_NAME']);
 $baseDir = ($scriptDir === '/' || $scriptDir === '\\') ? '' : $scriptDir;
 
-define('BASE_URL', rtrim($_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'], '/') . $baseDir . '/');
+define('BASE_URL', rtrim($_SERVER['REQUEST_SCHEME'] ?? $_SERVER['HTTP_X_FORWARDED_PROTO'] . '://' . $_SERVER['HTTP_HOST'], '/') . $baseDir . '/');
 
 
 require_once dirname(__DIR__) . '/routes/web.php';
